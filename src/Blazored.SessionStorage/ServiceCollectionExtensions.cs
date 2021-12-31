b53a4d89c0c089cb399a676ce60426c4a11af021
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-12-31 00:39:42 -0500

    Add support for different ServiceLifetime

--- src/Blazored.SessionStorage/ServiceCollectionExtensions.cs
+++ src/Blazored.SessionStorage/ServiceCollectionExtensions.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using Blazored.SessionStorage.JsonConverters;
 using Blazored.SessionStorage.Serialization;
 using Blazored.SessionStorage.StorageOptions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Blazored.SessionStorage
 {
@@ -13,18 +14,18 @@ namespace Blazored.SessionStorage
         public static IServiceCollection AddBlazoredSessionStorage(this IServiceCollection services)
             => AddBlazoredSessionStorage(services, null);
 
-        public static IServiceCollection AddBlazoredSessionStorage(this IServiceCollection services, Action<SessionStorageOptions> configure)
+        public static IServiceCollection AddBlazoredSessionStorage(this IServiceCollection services, Action<SessionStorageOptions> configure, ServiceLifetime servicesLifetime = ServiceLifetime.Scoped)
         {
-            return services
-                .AddScoped<IJsonSerializer, SystemTextJsonSerializer>()
-                .AddScoped<IStorageProvider, BrowserStorageProvider>()
-                .AddScoped<ISessionStorageService, SessionStorageService>()
-                .AddScoped<ISyncSessionStorageService, SessionStorageService>()
-                .Configure<SessionStorageOptions>(configureOptions =>
-                {
-                    configure?.Invoke(configureOptions);
-                    configureOptions.JsonSerializerOptions.Converters.Add(new TimespanJsonConverter());
-                });
+            services.TryAdd(new ServiceDescriptor(typeof(IJsonSerializer), typeof(SystemTextJsonSerializer), servicesLifetime));
+            services.TryAdd(new ServiceDescriptor(typeof(IStorageProvider), typeof(BrowserStorageProvider), servicesLifetime));
+            services.TryAdd(new ServiceDescriptor(typeof(ISessionStorageService), typeof(SessionStorageService), servicesLifetime));
+            services.TryAdd(new ServiceDescriptor(typeof(ISyncSessionStorageService), typeof(SessionStorageService), servicesLifetime));
+            services.Configure<SessionStorageOptions>(configureOptions =>
+            {
+                configure?.Invoke(configureOptions);
+                configureOptions.JsonSerializerOptions.Converters.Add(new TimespanJsonConverter());
+            });
+            return services;
         }
     }
 }
